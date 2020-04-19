25adee1628ff816d874bca462ed98bb015b207ec
vickiliang <62454045+vickiliang@users.noreply.github.com>
2020-04-19 11:19:21 +1000

    adding more to js

--- js/zdog-demo.js
+++ js/zdog-demo.js
@@ -1,4 +1,43 @@
+// Made with Zdog
 
+var yellow = '#ED0';
+var gold = '#EA0';
+var orange = '#E62';
+var garnet = '#C25';
+const TAU = Zdog.TAU;
+
+var illo = new Zdog.Illustration({
+  element: '.zdog-canvas',
+  dragRotate: true,
+  rotate: { x: -TAU/8 },
+});
+
+var burger = new Zdog.Anchor({
+  addTo: illo,
+  translate: { y: 24 },
+  rotate: { x: TAU/4 },
+});
+
+// top bun
+var topBun = new Zdog.Hemisphere({
+  addTo: burger,
+  diameter: 96,
+  translate: { z: 44 },
+  stroke: 24,
+  color: orange,
+  // backface: gold,
+});
+
+// cheese
+new Zdog.Rect({
+  addTo: burger,
+  width: 92,
+  height: 92,
+  translate: { z: 24 },
+  stroke: 16,
+  color: yellow,
+  fill: true,
+});
 
 // patty
 new Zdog.Ellipse({
@@ -52,4 +91,3 @@ function animate() {
 }
 
 animate();
- 
